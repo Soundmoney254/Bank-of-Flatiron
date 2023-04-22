f23c1d16be62520a95622977e96edb6a3f6d2ea6
Samuel <samnmbuguah@gmail.com>
2023-04-22 17:23:18 +0300

    Finished the fetch

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TableHeader from './TableHeader';
+import TableBody from './TableBody';
 
 function Table() {
   return (
    <table id='table'>
     <TableHeader/>
+    <TableBody/>
    </table>
   )
 }
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import TableRow from './TableRow';
+import React, { useState, useEffect } from 'react';
 
 function TableBody() {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:8001/transactions');
+        const data = await response.json();
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <tbody>
-
-   
+        {transactions.map(transaction => (
+            <tr key={transaction.id}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
+            </tr>
+        ))}
     </tbody>
   )
 }
