f623173f4be260517ad9cbc646738fb848417e62
Samuel <samnmbuguah@gmail.com>
2023-04-22 15:31:58 +0300

    Created the Header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header id='header'><h1>THE ROYAL BANK OF FLATIRON</h1> </header>
+  )
+}
+
+export default Header
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div><input id='search'/></div>
+  )
+}
+
+export default Search
