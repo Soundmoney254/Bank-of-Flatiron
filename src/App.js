1bce1c31a318e1fceecf06196a3c8bffec389877
Samuel <samnmbuguah@gmail.com>
2023-04-26 10:40:50 +0300

    Lifting State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import Form from './components/Form';
 import Table from './components/Table';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:8001/transactions');
+      const data = await response.json();
+      setTransactions(data.reverse());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  //Handler function for delete buttons
+  async function handleDelete(id) {
+    const confirmed = window.confirm('Are you sure you want to delete this transaction?');
+    if (confirmed) {
+      try {
+        await fetch(`http://localhost:8001/transactions/${id}`, {
+          method: 'DELETE',
+        });
+        setTransactions((prevState) =>
+          prevState.filter((transaction) => transaction.id !== id)
+        );
+      } catch (error) {
+        console.log(error); 
+      }
+    }
+  }
   return (
     <div className="App">
       <Header/>
@@ -13,7 +47,7 @@ function App() {
       <hr/>
       <Form/>
       <hr/>
-      <Table/>
+      <Table transactions={transactions} handleDelete ={handleDelete} />
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import TableHeader from './TableHeader';
-import TableBody from './TableBody';
 
-function Table() {
+function Table({transactions, handleDelete}) {
+
+
+  //Receives transactions state as a prop, maps and renders them
+  const allTransactions = transactions.map((transaction) => (
+    <tr key={transaction.id}>
+      <td><button className= "deleteButtons" onClick={() => handleDelete(transaction.id)}>Delete</button>{transaction.date}</td>
+      <td>{transaction.description}</td>
+      <td>{transaction.category}</td>
+      <td>{typeof transaction.amount === 'number' ? transaction.amount.toFixed(2) : '-'}</td>
+    </tr>
+  )) 
+
   return (
-   <table id='table' className="table table-striped table-hover">
-    <TableHeader/>
-    <TableBody/>
-   </table>
-  )
+    <table id='table' className="table table-striped table-hover">
+      <thead>
+          <tr>
+            <th className='tableHeaders'>Date</th>
+            <th className='tableHeaders'>Description</th>
+            <th className='tableHeaders'>Category</th>
+            <th className='tableHeaders'>Amount</th>
+        </tr>
+      </thead>
+      <tbody id='tableBody'>
+        {allTransactions}
+      </tbody>  
+    </table>
+  );
 }
-
 export default Table;
